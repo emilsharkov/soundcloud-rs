daf61b71b28a965fc7d193b9fa5addccf90631bb
Emil Sharkov <emosharkov@gmail.com>
2025-07-29 22:53:10 -0700

    add new methods

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,5 @@ use soundcloud_rs::{query::{SearchResultsQuery, SearchAllQuery, AlbumQuery}, Cli
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::new().await?;
-    let search_query = AlbumQuery {
-        q: Some("lil uzi vert".to_string()),
-        limit: Some(10),
-        offset: Some(0),
-        linked_partitioning: Some(true),
-    };
-    let search_response = client.search_albums(Some(&search_query)).await?;
-    println!("{:#?}", search_response);
     Ok(())
 } 
